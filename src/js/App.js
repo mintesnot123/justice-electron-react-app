1ca3219f7c10855b5198aecb0760b0878c75797c
Mintesnot Yismaw <yismawmintesnot@gmail.com>
2021-12-23 05:11:02 -0800

    Added form validation

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
@@ -9,6 +9,26 @@ import Stack from "react-bootstrap/Stack";
 import Card from "react-bootstrap/Card";
 
 export default function App() {
+  const [validated, setValidated] = useState(false);
+
+  const length1 = React.createRef();
+  const quantity1 = React.createRef();
+  const length2 = React.createRef();
+  const quantity2 = React.createRef();
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      event.preventDefault();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <>
       <Navbar bg="primary" variant="dark">
@@ -25,29 +45,66 @@ export default function App() {
             <Card.Title className="text-center">
               Minimum Rebar Needed Calculator
             </Card.Title>
-            <Form className="mt-3">
+            <Form
+              className="mt-3"
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+            >
               <Stack gap={2} className="mt-3">
                 <Row className="mb-3">
                   <Form.Group as={Col} controlId="length1">
                     <Form.Label>Length 1</Form.Label>
-                    <Form.Control type="number" placeholder="Length 1" />
+                    <Form.Control
+                      required
+                      type="number"
+                      placeholder="Length 1"
+                      ref={length1}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Length 1 is required!
+                    </Form.Control.Feedback>
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="quantity1">
                     <Form.Label>Quantity 1</Form.Label>
-                    <Form.Control type="number" placeholder="Quantity 1" />
+                    <Form.Control
+                      required
+                      type="number"
+                      placeholder="Quantity 1"
+                      ref={quantity1}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Quantity 1 is required!
+                    </Form.Control.Feedback>
                   </Form.Group>
                 </Row>
 
                 <Row className="mb-3">
                   <Form.Group as={Col} controlId="length2">
                     <Form.Label>Length 2</Form.Label>
-                    <Form.Control type="number" placeholder="Length 2" />
+                    <Form.Control
+                      required
+                      type="number"
+                      placeholder="Length 2"
+                      ref={length2}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Length 2 is required!
+                    </Form.Control.Feedback>
                   </Form.Group>
 
                   <Form.Group as={Col} controlId="quantity2">
                     <Form.Label>Quantity 2</Form.Label>
-                    <Form.Control type="number" placeholder="Quantity 2" />
+                    <Form.Control
+                      required
+                      type="number"
+                      placeholder="Quantity 2"
+                      ref={quantity2}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Quantity 2 is required!
+                    </Form.Control.Feedback>
                   </Form.Group>
                 </Row>
 
